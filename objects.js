d83615410e6fbf950d29b14f6f051cbd69c1ba5a
Chovin Carlson <chovin@guam.net>
2018-08-25 00:03:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,10 @@
 playlist = {}
 
 playlist['Young the Giant'] = "Islands"
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+}
